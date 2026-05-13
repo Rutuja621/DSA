d551362d13f90ef1e31465021afd953a2ed23abb
Rutuja <rutujakmisal@gmail.com>
2026-05-13 16:57:11 +0530

    Initial commit

---
+++ Array/array/OddIndexEle.java
@@ -0,0 +1,29 @@
+import java.util.*;
+public class OddIndexEle{
+	
+	public static void main(String [] arg){
+		Scanner sc=new Scanner(System.in);
+		
+		System.out.println("Enter a size of an array: ");
+		int size=sc.nextInt();
+		
+		int []arr=new int[size];
+		System.out.println("Enter array elements: ");
+		for(int i=0;i<arr.length;i++){
+			arr[i]=sc.nextInt();
+			
+		}
+		
+		for(int i=0;i<arr.length;i++){
+			if(i % 2 !=0){
+				System.out.print(arr[i]+" ");
+			}
+		}
+		
+
+
+
+	}
+
+
+}
