6970fad9d2ec783ad28677395cb077cd8f1837e8
Rutuja <rutujakmisal@gmail.com>
2026-07-07 16:16:36 +0530

    initial commit

--- Search/BinarySearch.java
+++ Search/BinarySearch.java
@@ -1,10 +1,8 @@
 import java.util.*;
 public class BinarySearch{
-
-
-
-
-
-
-
+	public static void main(String [] arg){
+		Scanner sc=new Scanner(System.in);
+		System.out.println("Enter a ");
+    
+	}
 }
---
+++ String/StringConstrctor.java
@@ -0,0 +1,6 @@
+public class StringConstrctor {
+
+    public static void main(String[] args) {
+        String str=new String();
+    }
+}
